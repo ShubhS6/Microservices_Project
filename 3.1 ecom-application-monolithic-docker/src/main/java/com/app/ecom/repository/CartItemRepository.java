51a2fe7f48d6292fd4c609828116e50756fdb5a9
ShubhS6 <shubhamvishwakarma732@gmail.com>
2026-06-21 01:08:31 +0530

    starting ms done with mysql docker compose

---
+++ 3.1 ecom-application-monolithic-docker/src/main/java/com/app/ecom/repository/CartItemRepository.java
@@ -0,0 +1,26 @@
+package com.app.ecom.repository;
+
+import com.app.ecom.model.CartItem;
+import com.app.ecom.model.Product;
+import com.app.ecom.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CartItemRepository extends JpaRepository<CartItem, Long> {
+    CartItem findByUserAndProduct(User user, Product product);
+
+    void deleteByUserIdAndProductId(String userId, String productId);
+
+    List<CartItem> findByUserId(String userId);
+
+    void deleteByUserId(String userId);
+
+    void deleteByUserAndProduct(User user, Product product);
+
+    List<CartItem> findByUser(User user);
+
+    void deleteByUser(User user);
+}
---
+++ 4 ecom-application-monolithic-sql/src/main/java/com/app/ecom/service/UserService.java
@@ -0,0 +1,82 @@
+package com.app.ecom.service;
+import com.app.ecom.dto.AddressDTO;
+import com.app.ecom.dto.UserRequest;
+import com.app.ecom.dto.UserResponse;
+import com.app.ecom.model.Address;
+import com.app.ecom.model.User;
+import com.app.ecom.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;
+   // private List<User> userList=new ArrayList<>();
+   // private Long nextId=1L;
+
+
+    public List<UserResponse> fetchAllUsers(){
+        return userRepository.findAll().stream().map(this::mapToUserResponse).collect(Collectors.toList());
+    }
+
+    public void addUser(UserRequest userRequest){
+        User user=new User();
+        UpdateUserFromRequest(user,userRequest);
+        userRepository.save(user);
+    }
+
+
+
+    public Optional<UserResponse> fetchUser(Long id) {
+        return userRepository.findById(id).map(this::mapToUserResponse);
+    }
+
+    public boolean updateUser(Long id,UserRequest UpdatedUserRequest){
+        return userRepository.findById(id)
+                .map(u->{UpdateUserFromRequest(u,UpdatedUserRequest);
+                    userRepository.save(u);
+                    return true;
+                }).orElse(false);
+    }
+
+    private UserResponse mapToUserResponse(User user){
+        UserResponse response=new UserResponse();
+        response.setId(String.valueOf(user.getId()));
+        response.setFirstName(user.getFirstName());
+        response.setLastName(user.getLastName());
+        response.setEmail(user.getEmail());
+        response.setPhone(user.getPhone());
+        response.setRole(user.getRole());
+        if(user.getAddress()!=null){
+            AddressDTO addressDTO=new AddressDTO();
+            addressDTO.setStreet(user.getAddress().getStreet());
+            addressDTO.setCity(user.getAddress().getCity());
+            addressDTO.setCountry(user.getAddress().getCountry());
+            addressDTO.setState(user.getAddress().getState());
+            addressDTO.setZipcode(user.getAddress().getZipcode());
+            response.setAddress(addressDTO);
+        }
+        return response;
+    }
+
+    private void UpdateUserFromRequest(User user, UserRequest userRequest) {
+        user.setFirstName(user.getFirstName());
+        user.setLastName(user.getLastName());
+        user.setPhone(userRequest.getPhone());
+        user.setEmail(user.getEmail());
+        if(userRequest.getAddress()!=null){
+            Address address=new Address();
+            address.setStreet(userRequest.getAddress().getStreet());
+            address.setState(userRequest.getAddress().getState());
+            address.setCity(userRequest.getAddress().getCity());
+            address.setCountry(userRequest.getAddress().getCountry());
+            address.setZipcode(userRequest.getAddress().getZipcode());
+            user.setAddress(address);
+        }
+    }
+}
