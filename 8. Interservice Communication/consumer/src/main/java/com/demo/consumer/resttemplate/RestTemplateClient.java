1ef4798f45d08bdc7efaeaf43ac2a77c4a9ddfee
ShubhS6 <shubhamvishwakarma732@gmail.com>
2026-06-24 20:45:48 +0530

    create bean of restTemplate

--- 8. Interservice Communication/consumer/src/main/java/com/demo/consumer/resttemplate/RestTemplateClient.java
+++ 8. Interservice Communication/consumer/src/main/java/com/demo/consumer/resttemplate/RestTemplateClient.java
@@ -1,12 +1,19 @@
 package com.demo.consumer.resttemplate;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
 
 @Service
+@RequiredArgsConstructor
 public class RestTemplateClient {
-    private static final String PROVIDED_URL
+
+    private final RestTemplate restTemplate;
+
+    private static final String PROVIDED_URL="http://localhost:8081";
 
     public String getInstanceInfo(){
-
+        return restTemplate.getForObject(PROVIDED_URL+"/instance-info",String.class);
     }
 }
