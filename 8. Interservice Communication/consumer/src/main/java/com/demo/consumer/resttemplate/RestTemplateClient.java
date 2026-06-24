ea938e440372410b85c6df37bb88c16e4bc127d1
ShubhS6 <shubhamvishwakarma732@gmail.com>
2026-06-24 20:37:28 +0530

    Rest Template config

---
+++ 8. Interservice Communication/consumer/src/main/java/com/demo/consumer/resttemplate/RestTemplateClient.java
@@ -0,0 +1,12 @@
+package com.demo.consumer.resttemplate;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class RestTemplateClient {
+    private static final String PROVIDED_URL
+
+    public String getInstanceInfo(){
+
+    }
+}
---
+++ 8. Interservice Communication/consumer/src/main/java/com/demo/consumer/resttemplate/RestTempplateConfig.java
@@ -0,0 +1,14 @@
+package com.demo.consumer.resttemplate;
+
+import org.springframework.boot.restclient.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class RestTempplateConfig {
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder){
+        return builder.build();
+    }
+}
