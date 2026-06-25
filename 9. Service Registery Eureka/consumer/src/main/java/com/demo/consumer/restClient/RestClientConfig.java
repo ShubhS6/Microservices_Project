0400af6a30d1cf8b35fa677ab10219ac58e0e4f3
ShubhS6 <shubhamvishwakarma732@gmail.com>
2026-06-25 20:07:45 +0530

    restClient load balance

--- 9. Service Registery Eureka/consumer/src/main/java/com/demo/consumer/restClient/RestClientConfig.java
+++ 9. Service Registery Eureka/consumer/src/main/java/com/demo/consumer/restClient/RestClientConfig.java
@@ -1,13 +1,21 @@
 package com.demo.consumer.restClient;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
 
 @Configuration
 public class RestClientConfig {
+
+    @Bean
+    @LoadBalanced
+    public RestClient.Builder loadb(){
+        return RestClient.builder();
+    }
+
     @Bean
     public RestClient restClient(RestClient.Builder builder){
-        return builder.baseUrl("http://localhost:8081").build();
+        return builder.baseUrl("http://provider").build();
     }
 }
